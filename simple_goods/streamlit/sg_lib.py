01a7d3b3f9f50721528acfc98b41f4b1fbfcbb46
Антон Суетов <delffine555@gmail.com>
2025-02-17 05:18:32 +0000

    2002

--- simple_goods/streamlit/sg_lib.py
+++ simple_goods/streamlit/sg_lib.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import os
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime
 #from matplotlib import colors
